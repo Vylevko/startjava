14b43922547364800c788bea316ebb9f7e41b55f
Yury Vylevko <Vylevko@gmail.com>
2021-01-12 21:12:06 +0300

    Changed performed after chimax review.

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,20 +1,21 @@
 public class Variable {
 	public static void main(String[] args) {
-	byte coreNum=6;
-	short maxMemory=128;
-	int  oneCoreFr=1800000000;
-	long hdVolume=1111111111111l;
-	float mem=41.6f;
-	double eMath=2.71828182845904d;
-	char coreModel='C';
-	boolean itWorks=true;
-	System.out.println("coreNum "+coreNum);
-	System.out.println("maxMemory "+maxMemory);
-	System.out.println("oneCoreFr "+oneCoreFr);
-	System.out.println("hdVolume "+hdVolume);
-	System.out.println("mem "+mem);
-	System.out.println("eMath "+eMath);
-	System.out.println("coreModel "+coreModel);
-	System.out.println("itWorks? "+itWorks);
+		byte coreNum = 6;
+		short maxMemory = 128;
+		int oneCoreFr = 1800000000;
+		long hdVolume = 1111111111111l;
+		float mem = 41.6f;
+		double eMath = 2.71828182845904d;
+		char coreModel = 'C';
+		boolean itWorks=true;
+		
+		System.out.println("coreNum "+coreNum);
+		System.out.println("maxMemory "+maxMemory);
+		System.out.println("oneCoreFr "+oneCoreFr);
+		System.out.println("hdVolume "+hdVolume);
+		System.out.println("mem "+mem);
+		System.out.println("eMath "+eMath);
+		System.out.println("coreModel "+coreModel);
+		System.out.println("itWorks? "+itWorks);
 	}
 }
