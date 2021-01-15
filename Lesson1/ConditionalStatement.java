a1cf24e01a73fe0b6cdd7b343ebcac7e62ee4f9f
Yury Vylevko <Vylevko@gmail.com>
2021-01-15 23:51:18 +0300

    Added Unicode.java like lesson1.task11 Changed tab to space in all files Changed MyGame after chimax review

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,33 +1,33 @@
 public class ConditionalStatement {
-	public static void main(String[] args) {
-		byte age = 33;
-		if (age > 30) {
-			System.out.println("Not a Kid already");	
-		}
-		
-		boolean isMale = true;
-		if (isMale) {
-			System.out.println("Guy");
-		}
-		
-		if (!isMale) {
-			System.out.println("Maybe a girl?");
-		}
-		
-		float height = 1.83f;
-		if (height < 1.80) {
-			System.out.println("Shorter than average");
-		} else {
-			System.out.println("Average or higher");	
-		}
-		
-		char firstNameLetter = 'M';
-		if (firstNameLetter == 'M') {
-			System.out.println("Mihkail?");
-		} else if (firstNameLetter == 'I') {
-			System.out.println("Irina?");
-		} else {
-			System.out.println("Other name");
-		}
-	}
+    public static void main(String[] args) {
+        byte age = 33;
+        if (age > 30) {
+            System.out.println("Not a Kid already");    
+        }
+        
+        boolean isMale = true;
+        if (isMale) {
+            System.out.println("Guy");
+        }
+        
+        if (!isMale) {
+            System.out.println("Maybe a girl?");
+        }
+        
+        float height = 1.83f;
+        if (height < 1.80) {
+            System.out.println("Shorter than average");
+        } else {
+            System.out.println("Average or higher");    
+        }
+        
+        char firstNameLetter = 'M';
+        if (firstNameLetter == 'M') {
+            System.out.println("Mihkail?");
+        } else if (firstNameLetter == 'I') {
+            System.out.println("Irina?");
+        } else {
+            System.out.println("Other name");
+        }
+    }
 }
--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,26 +1,26 @@
 public class Cycle {
-	public static void main(String[] args) {
-		System.out.println("For Cycle");
-		for (int i = 0; i <= 20; i++) {
-			System.out.println("number " + i);	
-		}
-		
-		System.out.println("While Cycle");
-		int a = 6;
-		while (a >= -6) {
-			System.out.println("number " + a);
-			a -= 2;
-		}
-		
-		System.out.println("DO While Cycle");
-		int sum = 0;
-		a = 10;
-		do { 
-			if (a % 2 == 1) {
-				sum += a;
-			}
-			a++;
-		} while (a <= 20);
-		System.out.println("DO While Cycle result " + sum);
-	}
+    public static void main(String[] args) {
+        System.out.println("For Cycle");
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("number " + i);  
+        }
+        
+        System.out.println("While Cycle");
+        int a = 6;
+        while (a >= -6) {
+            System.out.println("number " + a);
+            a -= 2;
+        }
+        
+        System.out.println("DO While Cycle");
+        int sum = 0;
+        a = 10;
+        do { 
+            if (a % 2 == 1) {
+                sum += a;
+            }
+            a++;
+        } while (a <= 20);
+        System.out.println("DO While Cycle result " + sum);
+    }
 }
