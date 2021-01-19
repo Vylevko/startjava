6fe0520416169f7528ca4ddfc04bcd888b40e701
Yury Vylevko <Vylevko@gmail.com>
2021-01-19 22:44:28 +0300

    lesson2.task5

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,27 +1,67 @@
 public class Wolf {
-	boolean isMail;
-	String nickName;
-	int weight;
-	int age;
-	String color;
-	
-	void walk() {
-		System.out.println("Walking");
-	}
+    private boolean isMail;
+    private String nickName;
+    private int weight;
+    private int age;
+    private String color;
+    
+    public void walk() {
+        System.out.println("Walking");
+    }
 
-	void sit() {
-		System.out.println("Sitting");
-	}
+    public void sit() {
+        System.out.println("Sitting");
+    }
 
-	void run() {
-		System.out.println("Runing");
-	}
-	
-	void howl() {
-		System.out.println("Howling");
-	}
-	
-	void hunt() {
-		System.out.println("Hunting");
-	}
+    public void run() {
+        System.out.println("Runing");
+    }
+    
+    public void howl() {
+        System.out.println("Howling");
+    }
+    
+    public void hunt() {
+        System.out.println("Hunting");
+    }
+    
+    public void setIsMail(boolean isMail) {
+        this.isMail = isMail;
+    }
+    
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public boolean getIsMail() {
+        return isMail;
+    }
+    
+    public String getNickName() {
+        return nickName;
+    }
+    
+    public int getWeight() {
+        return weight;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public String getColor() {
+        return color;
+    }
 }
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,21 +1,26 @@
 public class WolfTest {
     public static void main (String[] args) {
         Wolf wolfFirst = new Wolf();
-        wolfFirst.isMail = false;
-        wolfFirst.age = 1;
-        wolfFirst.color = "White";
-        wolfFirst.nickName = "SherstyanoyVol4ara";
-        wolfFirst.weight = 25;
+        wolfFirst.setIsMail(false);
+        int age = 7;
+        if (age > 8) {
+            System.out.println("Invalid age");
+        } else { 
+            wolfFirst.setAge(age);
+        }
+        wolfFirst.setColor("White");
+        wolfFirst.setNickName("SherstyanoyVol4ara");
+        wolfFirst.setWeight(25);
         
-        if (wolfFirst.isMail) {
+        if (wolfFirst.getIsMail()) {
             System.out.println("Good Boy");
         } else {
             System.out.println("Good Girl");
         }
-        System.out.println("Age = " + wolfFirst.age);
-        System.out.println("Color = " + wolfFirst.color);
-        System.out.println("Name = " + wolfFirst.nickName);
-        System.out.println("Weight = " + wolfFirst.weight);
+        System.out.println("Age = " + wolfFirst.getAge());
+        System.out.println("Color = " + wolfFirst.getColor());
+        System.out.println("Name = " + wolfFirst.getNickName());
+        System.out.println("Weight = " + wolfFirst.getWeight());
         
         wolfFirst.howl();
         wolfFirst.sit();
