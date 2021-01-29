707b1d1915b6997e4a6fcf4e0b7a7de02a51a66d
Yury Vylevko <Vylevko@gmail.com>
2021-01-29 23:04:06 +0300

    cahnged GuessNumber

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class GuessNumber {
     private int generatedNumber;
+    private int number;
     private Player player1;
     private Player player2;
     Random random = new Random();
@@ -18,47 +19,49 @@ public class GuessNumber {
         System.out.println("Computer generated valie from the range (0,100]");
         do { 
             System.out.println(player1.getName() + ", Your turn, insert value from the range (0,100]");
-            if (scan.hasNextInt()) {
-                player1.setNumber(scan.nextInt());
-                scan.nextLine();
-            } else {
-                player1.setNumber(0);
-                scan.nextLine();
-            }
+            player1.setNumber(insertNumber());
             
             if (guessNumber(player1.getNumber())) {
                 System.out.println(player1.getName() + " Won!!!! GZ");
                 break;
             }   
             System.out.println(player2.getName() + ", Your turn, insert value from the range (0,100]");
-            if (scan.hasNextInt()) {
-                player2.setNumber(scan.nextInt());
-                scan.nextLine();
-            } else {
-                player1.setNumber(0);
-                scan.nextLine();
-            }
+            player2.setNumber(insertNumber());
             
             if (guessNumber(player2.getNumber())) {
                 System.out.println(player2.getName() + " Won!!!! GZ");
                 break;
             }
-            //System.out.println(generatedNumber);
+            System.out.println(generatedNumber);
         } while (true);
         
     }
 
-    private boolean guessNumber(int Number) {
-        if (!(Number > 0 && Number <= 100)) {
-            System.out.println("Inserted value is out the range (0,100]");
-        } else if (Number > generatedNumber) {
-            System.out.println("Generated number is less than yourth");
-        } else if (Number < generatedNumber) {
-            System.out.println("Generated number is greater than yourth");
-        } else {
-            System.out.println("Correct");
-            return true;
+    private boolean guessNumber(int number) {
+        if (number != 0) {
+            if (number > generatedNumber) {
+                System.out.println("Generated number is less than yourth");
+            } else if (number < generatedNumber) {
+                System.out.println("Generated number is greater than yourth");
+            } else {
+                System.out.println("Correct");
+                return true;
+            }
         }
-    return false;
+        return false;
     }
+    
+    private int insertNumber() {
+        if (scan.hasNextInt()) {
+            number = scan.nextInt();
+            scan.nextLine();
+        } else {
+            number = 0;
+            scan.nextLine();
+        }
+        if(!(number > 0 && number <= 100)) {
+            System.out.println("Inserted value is out the range (0,100]");
+        }
+            return number;
+    }   
 }
