9269bc3ea58eb3bba8dc50f8d0473ebac9f9cab9
Yury Vylevko <Vylevko@gmail.com>
2021-02-04 23:09:24 +0300

    lesson3.task5

--- src/com/startjava/lesson_2_3/robot/JaegerTest.java
+++ src/com/startjava/lesson_2_3/robot/JaegerTest.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.robot;
+
 public class JaegerTest {
     public static void main(String[] args) {
         Jaeger jaeger1 = new Jaeger("Striker Eureka","Mark-5","Australia",1.85f,76.2f,10,10,9);
